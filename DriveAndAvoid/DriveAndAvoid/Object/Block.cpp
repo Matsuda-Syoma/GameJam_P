ce0bf73c27f804093d1b21c1b853a4ac5d4ee63f
Matsuda-Syoma <syoma-matsuda-k22f@stu.kbc.ac.jp>
2024-02-13 17:29:34 +0900

    block調整

--- DriveAndAvoid/DriveAndAvoid/Object/Block.cpp
+++ DriveAndAvoid/DriveAndAvoid/Object/Block.cpp
@@ -13,12 +13,22 @@ void Block::Initialize(int x, int y)
 {
 	location = Vector2D((float)x * 32, (float)y * 32);
 	box_size = Vector2D(32.0f, 32.0f);
+
+	//画像の読み込み
+	image = LoadGraph("Resource/images/block1.bmp");
+
+	//エラーチェック
+	if (image == -1)
+	{
+		throw("Resource/images/block1.bmpがありません。\n");
+	}
 }
 
 void Block::Draw() const
 {
 	// 画像の描画
 	DrawBox(location.x, location.y, location.x + box_size.x, location.y + box_size.y, 0xffffff, true);
+	DrawGraph(location.x, location.y, image, TRUE);
 }
 
 void Block::Finalize()
