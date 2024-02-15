239d0c1e3d1157ff9a16c9575b4f29d891a00d55
Matsuda-Syoma <syoma-matsuda-k22f@stu.kbc.ac.jp>
2024-02-15 14:05:54 +0900

    sound追加

--- DriveAndAvoid/DriveAndAvoid/Utility/Sounds.h
+++ DriveAndAvoid/DriveAndAvoid/Utility/Sounds.h
@@ -2,10 +2,17 @@
 
 class Sounds
 {
-private:
 
 
 public:
-	Sounds();						// コンストラクタ
+	static int BGM_Game;
+	static int BGM_Title;
+	static int SE_Break;
+	static int SE_Shoot;
+	static int SE_Hit[3];
+	static int SE_PlayerHit;
+
+
+	static void LoadSounds();						// コンストラクタ
 	~Sounds();						// デストラクタ
 };
